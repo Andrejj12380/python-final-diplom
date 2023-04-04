2442242571def87615c24c3cced267419917eb16
Andrey Khudoyarov <andrei.hudojaroff@yandex.ru>
2023-04-04 09:36:35 +0400

    add docs to serializers

--- orders/backend_orders/serializers.py
+++ orders/backend_orders/serializers.py
@@ -4,6 +4,14 @@ from .models import User, Category, Shop, ProductInfo, Product, ProductParameter
 
 
 class ContactSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели контакта.
+    Атрибуты:
+        model (Contact): модель контакта
+        fields (tuple): поля сериализации
+        read_only_fields (tuple): только для чтения поля
+        extra_kwargs (dict): дополнительные аргументы
+    """
     class Meta:
         model = Contact
         fields = ('id', 'city', 'street', 'house', 'structure', 'building', 'apartment', 'user', 'phone')
@@ -14,6 +22,13 @@ class ContactSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели пользователя.
+    Атрибуты:
+        model (User): модель пользователя
+        fields (tuple): поля сериализации
+        read_only_fields (tuple): только для чтения поля
+    """
     contacts = ContactSerializer(read_only=True, many=True)
 
     class Meta:
@@ -23,6 +38,13 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели категории.
+    Атрибуты:
+        model (Category): модель категории
+        fields (tuple): поля сериализации
+        read_only_fields (tuple): только для чтения поля
+    """
     class Meta:
         model = Category
         fields = ('id', 'name',)
@@ -30,6 +52,13 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ShopSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели магазина.
+    Атрибуты:
+        model (Shop): модель магазина
+        fields (tuple): поля сериализации
+        read_only_fields (tuple): только для чтения поля
+    """
     class Meta:
         model = Shop
         fields = ('id', 'name', 'state',)
@@ -37,6 +66,13 @@ class ShopSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели продукта.
+    Атрибуты:
+        model (Product): модель продукта
+        fields (tuple): поля сериализации
+        category (StringRelatedField): категория продукта
+    """
     category = serializers.StringRelatedField()
 
     class Meta:
@@ -45,6 +81,13 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ProductParameterSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели параметра продукта.
+    Атрибуты:
+        model (ProductParameter): модель параметра продукта
+        fields (tuple): поля сериализации
+        parameter (StringRelatedField): параметр продукта
+    """
     parameter = serializers.StringRelatedField()
 
     class Meta:
@@ -53,6 +96,15 @@ class ProductParameterSerializer(serializers.ModelSerializer):
 
 
 class ProductInfoSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор модели информации о продукте.
+    Атрибуты:
+        product (ProductSerializer): сериализатор продукта, связанного с информацией о продукте
+        product_parameters (ProductParameterSerializer): сериализатор списка параметров продукта
+        model (str): модель продукта
+        fields (tuple): поля сериализации
+        read_only_fields (tuple): только для чтения поля
+    """
     product = ProductSerializer(read_only=True)
     product_parameters = ProductParameterSerializer(read_only=True, many=True)
 
@@ -63,6 +115,14 @@ class ProductInfoSerializer(serializers.ModelSerializer):
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели элемента заказа.
+    Атрибуты:
+        model (OrderItem): модель элемента заказа
+        fields (tuple): поля сериализации
+        read_only_fields (tuple): только для чтения поля
+        extra_kwargs (dict): дополнительные настройки полей
+    """
     class Meta:
         model = OrderItem
         fields = ('id', 'product_info', 'quantity', 'order',)
@@ -73,10 +133,22 @@ class OrderItemSerializer(serializers.ModelSerializer):
 
 
 class OrderItemCreateSerializer(OrderItemSerializer):
+    """
+    Сериализатор для создания элемента заказа.
+    Атрибуты:
+        product_info (ProductInfoSerializer): сериализатор информации о продукте (только для чтения)
+    """
     product_info = ProductInfoSerializer(read_only=True)
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели заказа.
+    Атрибуты:
+        model (Order): модель заказа
+        fields (tuple): поля сериализации
+        read_only_fields (tuple): только для чтения поля
+    """
     ordered_items = OrderItemCreateSerializer(read_only=True, many=True)
 
     total_sum = serializers.IntegerField()
